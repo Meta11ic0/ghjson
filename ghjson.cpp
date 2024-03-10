797752240d485ced5c1570a8e1f8733d2b924096
Meta11ic0 <ihaveabigdoor@gmail.com>
2024-03-10 22:27:33 +0800

    new

--- ghjson.cpp
+++ ghjson.cpp
@@ -4,10 +4,14 @@ namespace ghjson
 {
     /* * * * * * * * * * * * * * * * * * * */    
     /* Helper for representing null - just a do-nothing struct, plus comparison
-     * operators so the helpers in JsonValue work. We can't use nullptr_t because
-     * it may not be orderable.
-     */
-    struct NullStruct{};
+    /* operators so the helpers in JsonValue work. We can't use nullptr_t because
+    /* it may not be orderable.* * * * * * */
+    class NullClass
+    {
+        public:
+            bool operator == (NullClass) {return true;}
+            bool operator < (NullClass) {return false;}
+    };
     /* * * * * * * * * * * * * * * * * * * */
    
     template<JsonType tag, typename T>
@@ -17,79 +21,130 @@ namespace ghjson
             explicit Value(const T &value) : m_value(value) {};
             explicit Value(T &&value)      : m_value(move(value)) {};
             // Get type tag
-            JsonType type() const override 
+            const JsonType type() const override 
             {
                 return tag;
             }
-
+            // Comparisons 因为是JsonValue的指针
+            //所以需要用static_cast转换成Value类型指针才能访问m_value
+            bool equals(const JsonValue * other) const override 
+            {
+                return m_value == static_cast<const Value<tag, T> *>(other)->m_value;
+            }
+            bool less(const JsonValue * other) const override 
+            {
+                return m_value <  static_cast<const Value<tag, T> *>(other)->m_value;
+            }   
             const T m_value;
     };
 
-    class JsonNull final : public Value<JsonType::NUL, NullStruct> 
+    class JsonNumber final : public Value<JsonType::NUMBER, double>
+    {
+        public:
+            explicit JsonNumber(double value) : Value(value) {}
+            explicit JsonNumber(int value) : Value(value) {}
+        private:
+            const double number_value() const override { return m_value; }
+            const int int_value() const override { return m_value; }
+            bool equals(const JsonValue * other) const override { return m_value == other->number_value(); }
+            bool less(const JsonValue * other)   const override { return m_value <  other->number_value(); }
+    };
+
+    class JsonBoolean final : public Value<JsonType::BOOL, bool>
+    {
+        public:
+            explicit JsonBoolean(bool value) : Value(value){}
+        private:
+            const bool bool_value() const override {return m_value;}
+    };
+
+    class JsonString final : public Value<JsonType::STRING, string>
+    {
+        public:
+            explicit JsonString(string &&value) : Value(move(value)) {}
+        private:
+            const string &string_value() const override { return m_value;}
+
+    };
+
+    class JsonArray final : public Value<JsonType::ARRAY, ghjson::array>
+    {
+
+    };
+
+    class JsonObject final : public Value<JsonType::OBJECT, ghjson::object>
+    {
+
+    };
+
+    class JsonNull final : public Value<JsonType::NUL, NullClass> 
     {
         public:
             JsonNull() : Value({}) {};
     };
 
-    class JsonNumber : public Value<JsonType::NUMBER, double>
-    {
-
-    };
-
-    class JsonBool : public Value<JsonType::BOOL, bool>
-    {
-
-    };
-
-    class JsonString : public Value<JsonType::STRING, string>
-    {
-
-    };
-
-    class JsonArray : public Value<JsonType::ARRAY, ghjson::array>
-    {
-
-    };
-
-    class JsonObject: public Value<JsonType::OBJECT, ghjson::object>
-    {
-
-    };
-
     /* * * * * * * * * * * * * * * * * * * */
     /* Static globals - static-init-safe * */
-
-    class Statics
+    class Static_JsonValues
     {
         public:
-            std::shared_ptr<JsonValue> null = make_shared<JsonNull>();
-            std::shared_ptr<JsonValue> t = make_shared<JsonBoolean>(true);
-            std::shared_ptr<JsonValue> f = make_shared<JsonBoolean>(false);
-            string empty_string;
-            vector<Json> empty_vector;
-            map<string, Json> empty_map;
-            Statics(){};
+            const shared_ptr<JsonValue> null = make_shared<JsonNull>();
+            const shared_ptr<JsonValue> t    = make_shared<JsonBoolean>(true);
+            const shared_ptr<JsonValue> f    = make_shared<JsonBoolean>(false);
+            const string                       empty_string;
+            const array                empty_vector;
+            const object           empty_map;
+            Static_JsonValues(){};
     };
+    static const Static_JsonValues & Get_Static_JsonValues() 
+    {
+        static const Static_JsonValues s {};
+        return s;
+    }
+    static const Json & Get_Static_Json_Null()
+    {
+        static const Json json_null;
+        return json_null;
+    }
+    //我们将Get_Static_Json_Null函数单独设置出来，
+    //以确保在第一次需要一个类型为Json且表示null的对象时安全地创建它。
+    //我们避免的问题是Json构造函数依赖犹未构建完成的Static_JsonValues().null，
+    //于是设计了一种方法，可以在不引起依赖问题的情况下，安全并按需初始化全局Json null对象。
     /* * * * * * * * * * * * * * * * * * * */      
 
-    /* * * * * * * * * * * * * * * * * * * *
-    * class Json* * * * * * * * * * * * * *
-    * * * * * * * * * * * * * * * * * * * */ 
-    const JsonType Json::type()                  const{return m_ptr->type();        }
-    const double   Json::number_value()          const{return m_ptr->number_value();}
-    const bool     Json::bool_value()            const{return m_ptr->bool_value();  }
-    const const    string & Json::string_value() const{return m_ptr->string_value();}
-    const array &  Json::array_items()           const{return m_ptr->array_items(); }
-    const object & Json::object_items()          const{return m_ptr->object_items();}
+
+    /* class Json* * * * * * * * * * * * * */
+    /* Constructors*/
+    Json::Json() noexcept                  : m_ptr(Get_Static_JsonValues().null) {}
+    Json::Json(std::nullptr_t) noexcept    : m_ptr(Get_Static_JsonValues().null) {}
+    Json::Json(double value)               : m_ptr(make_shared<JsonNumber>(value)) {}
+    Json::Json(int value)                  : m_ptr(make_shared<JsonNumber>(value)) {}
+    Json::Json(bool value)                 : m_ptr(value ? Get_Static_JsonValues().t : Get_Static_JsonValues().f) {}
+    Json::Json(const string &value)        : m_ptr(make_shared<JsonString>(value)) {}
+    Json::Json(string &&value)             : m_ptr(make_shared<JsonString>(move(value))) {}
+    Json::Json(const char * value)         : m_ptr(make_shared<JsonString>(value)) {}
+    Json::Json(const array &values)        : m_ptr(make_shared<JsonArray>(values)) {}
+    Json::Json(array &&values)             : m_ptr(make_shared<JsonArray>(move(values))) {}
+    Json::Json(const object &values)       : m_ptr(make_shared<JsonObject>(values)) {}
+    Json::Json(object &&values)            : m_ptr(make_shared<JsonObject>(move(values))) {}
+
+
+    const JsonType Json::type()                           const{return m_ptr->type();        }
+    const double   Json::number_value()                   const{return m_ptr->number_value();}
+    const int      Json::int_value()                      const{return m_ptr->number_value();}
+    const bool     Json::bool_value()                     const{return m_ptr->bool_value();  }
+    const const    string & Json::string_value()          const{return m_ptr->string_value();}
+    const array &  Json::array_items()                    const{return m_ptr->array_items(); }
+    const object & Json::object_items()                   const{return m_ptr->object_items();}
     /* * * * * * * * * * * * * * * * * * * *
     * class JsonValue * * * * * * * * * * *
     * * * * * * * * * * * * * * * * * * * */ 
-    double         JsonValue::number_value()              const { return 0; }
-    int            JsonValue::int_value()                 const { return 0; }
-    bool           JsonValue::bool_value()                const { return false; }
-    const string & JsonValue::string_value()              const { return statics().empty_string; }
-    const array &  JsonValue::array_items()               const { return statics().empty_vector; }
-    const object & JsonValue::object_items()              const { return statics().empty_map; }
-    const Json &   JsonValue::operator[] (size_t)         const { return static_null(); }
-    const Json &   JsonValue::operator[] (const string &) const { return static_null(); }    
+    const double   JsonValue::number_value()              const { return 0; }
+    const int      JsonValue::int_value()                 const { return 0; }
+    const bool     JsonValue::bool_value()                const { return false; }
+    const string & JsonValue::string_value()              const { return Get_Static_JsonValues().empty_string; }
+    const array &  JsonValue::array_items()               const { return Get_Static_JsonValues().empty_vector; }
+    const object & JsonValue::object_items()              const { return Get_Static_JsonValues().empty_map; }
+    const Json &   JsonValue::operator[] (size_t)         const { return Get_Static_Json_Null(); }
+    const Json &   JsonValue::operator[] (const string &) const { return Get_Static_Json_Null(); }    
 } 
--- ghjson.hpp
+++ ghjson.hpp
@@ -10,6 +10,8 @@ namespace ghjson
     using std::string;
     using std::vector;
     using std::map;
+    using std::shared_ptr;
+    using std::make_shared;
 
     enum class JsonType
     {
@@ -23,10 +25,13 @@ namespace ghjson
     {
         public:
             virtual const JsonType type() const = 0;
+            //要提供比较函数
+            virtual bool equals(const JsonValue * other) const = 0;
+            virtual bool less(const JsonValue * other) const = 0;
             virtual const string dump() const = 0;
-            virtual double number_value() const;
-            virtual int int_value() const; 
-            virtual bool bool_value() const;
+            virtual const double number_value() const;
+            virtual const int int_value() const; 
+            virtual const bool bool_value() const;
             virtual const string &string_value() const;
             virtual const array &array_items() const;
             virtual const Json &operator[](size_t i) const;
@@ -40,6 +45,37 @@ namespace ghjson
     class Json
     {
         public:
+
+            /*noexcept关键字在C++中用来指定一个函数不会抛出异常。
+            当用在构造函数上时，它表明构造函数调用过程中不会抛出任何异常，
+            这样的构造函数通常具有更高的性能，因为编译器可以做出额外优化，例如更有效地使用移动语义而不是复制。
+            在Json类中，NUL类型的构造函数被标记为noexcept，
+            因为它们实际上执行的操作非常简单：它们只是初始化内部的指针成员为null，没有进行任何可能抛出异常的操作
+            如NUMBER、BOOL、STRING、ARRAY和OBJECT, 
+            例如字符串的构造可能涉及到内存的分配，
+            而这个操作是有可能失败并抛出异常的，
+            所以它们的构造函数不应标记为noexcept。
+            相似地，向vector添加元素（在ARRAY类型中）
+            和向map添加键值对（在OBJECT类型中）都可能引起内存分配错误，
+            并携带异常抛出的风险。
+            因此，只有那些保证在任何情况下都不会触发异常抛出的构造函数才应该被声明为noexcept。
+            这是为了清晰地表明它们的异常安全保证，
+            以及允许编译器进行优化
+            */
+            // Constructors for the various types of JSON value.
+            Json() noexcept;                // NUL
+            Json(std::nullptr_t) noexcept;  // NUL
+            Json(double value);             // NUMBER
+            Json(int value);                // NUMBER
+            Json(bool value);               // BOOL
+            Json(const std::string &value); // STRING
+            Json(std::string &&value);      // STRING
+            Json(const char * value);       // STRING
+            Json(const array &values);      // ARRAY
+            Json(array &&values);           // ARRAY
+            Json(const object &values);     // OBJECT
+            Json(object &&values);          // OBJECT
+
             Json Parse(const string & str);
             string dump() const;
             const JsonType type() const;
@@ -47,6 +83,7 @@ namespace ghjson
             // distinguish between integer and non-integer numbers - number_value() and int_value()
             // can both be applied to a NUMBER-typed object.
             const double number_value() const;
+            const int int_value() const;
             //int int_value() const;
             // Return the enclosed value if this is a boolean, false otherwise.
             const bool bool_value() const;
